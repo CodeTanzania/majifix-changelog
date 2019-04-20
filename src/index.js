831676b4afb172d7064a33a6d9037185ae7037c0
Benson Maruchu <benmaruchu@gmail.com>
2019-04-20 10:12:09 +0300

    feat: expose app, apiVersion, model and router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-// import path from 'path';
 // import app from '@lykmapipo/express-common';
-// import _ from 'lodash';
-// import pkg from '../package.json';
+import app from '@lykmapipo/express-common';
+import _ from 'lodash';
+import pkg from '../package.json';
+import Changelog from './changelog.model';
+import router from './http.router';
 
-// /* declarations */
-// const fields = [
-//   'name',
-//   'description',
-//   'version',
-//   'license',
-//   'homepage',
-//   'repository',
-//   'bugs',
-//   'sandbox',
-//   'contributors',
-// ];
+/* declarations */
+const fields = [
+  'name',
+  'description',
+  'version',
+  'license',
+  'homepage',
+  'repository',
+  'bugs',
+  'sandbox',
+  'contributors',
+];
 
-// /* extract information from package.json */
-// export const info = _.merge({}, _.pick(pkg, fields));
+/* extract information from package.json */
+const info = _.merge({}, _.pick(pkg, fields));
 
-// export const apiVersion = router.version;
+/* extract api version from router version */
+const apiVersion = router.version;
+
+/* bind changelog router */
+app.mount(router);
+
+export { app, apiVersion, info, Changelog, router };
